cb9f7278093886ded4910bc9c18233ea0d3026ca
Aleksandr Soloshenko <capcom@soft-c.ru>
2021-11-28 19:30:00 +0700

    Added: video storage azure

---
+++ video-storage-azure/go.mod
@@ -0,0 +1,22 @@
+module github.com/capcom6/go-tube/video-storage-azure
+
+go 1.17
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.2.0
+	github.com/gofiber/fiber/v2 v2.22.0
+	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.20.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.8.1 // indirect
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ video-storage-azure/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/capcom6/go-tube/video-storage-azure/config"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	port := config.Config("PORT")
+	storageAccountName := config.Config("STORAGE_ACCOUNT_NAME")
+	storageAccessKey := config.Config("STORAGE_ACCESS_KEY")
+	if port == "" {
+		panic("Please set PORT enviroment variable.")
+	}
+	if storageAccountName == "" {
+		panic("Please set STORAGE_ACCOUNT_NAME enviroment variable.")
+	}
+	if storageAccessKey == "" {
+		panic("Please set STORAGE_ACCESS_KEY enviroment variable.")
+	}
+
+	app := fiber.New()
+
+	app.Get("/video", func(c *fiber.Ctx) error {
+		filename := c.Query("path")
+		if filename == "" {
+			return c.SendStatus(404)
+		}
+
+		cred, err := azblob.NewSharedKeyCredential(storageAccountName, storageAccessKey)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		serviceClient, err := azblob.NewServiceClientWithSharedKey(fmt.Sprintf("https://%s.blob.core.windows.net/", storageAccountName), cred, nil)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		container := serviceClient.NewContainerClient("videos")
+		blockBlob := container.NewBlockBlobClient(filename)
+
+		ctx := context.Background()
+		properties, err := blockBlob.GetProperties(ctx, &azblob.GetBlobPropertiesOptions{})
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		c.Set("Content-Type", *properties.ContentType)
+		c.Set("Accept-Ranges", "bytes")
+
+		length := int64(0)
+		downloadOption := azblob.DownloadBlobOptions{}
+		bytesRange, err := c.Range(int(*properties.ContentLength))
+		if err != nil {
+			length = *properties.ContentLength
+			c.Status(200)
+		} else {
+			// support of single range only
+			begin, end := int64(bytesRange.Ranges[0].Start), int64(bytesRange.Ranges[0].End)
+			length = end - begin + 1
+
+			downloadOption.Offset = &begin
+			downloadOption.Count = &length
+
+			c.Status(206)
+			c.Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", bytesRange.Ranges[0].Start, bytesRange.Ranges[0].End, *properties.ContentLength))
+		}
+
+		get, err := blockBlob.Download(ctx, &downloadOption)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		reader := get.Body(azblob.RetryReaderOptions{})
+
+		return c.SendStream(reader, int(length))
+	})
+
+	log.Fatal(app.Listen(":" + port))
+}
