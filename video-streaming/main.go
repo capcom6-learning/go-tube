8de363d79584639fa3a6616440d7ecd9e8fef123
Aleksandr Soloshenko <capcom@soft-c.ru>
2021-12-01 23:10:18 +0700

    Added: MongoDB

--- video-streaming/main.go
+++ video-streaming/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,16 @@ import (
 
 	"github.com/capcom6/go-tube/video-streaming/config"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type VideoRecord struct {
+	ID   primitive.ObjectID `bson:"_id"`
+	Path string             `bson:"videoPath"`
+}
 
 func main() {
 	port := config.Config("PORT")
@@ -29,10 +39,44 @@ func main() {
 		panic("VIDEO_STORAGE_PORT enviroment variable not set")
 	}
 
+	dbhost := config.Config("DBHOST")
+	dbname := config.Config("DBNAME")
+	if dbhost == "" {
+		panic("DBHOST enviroment variable not set")
+	}
+	if dbname == "" {
+		panic("DBNAME enviroment variable not set")
+	}
+
+	mongodb, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbhost))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := mongodb.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	collection := mongodb.Database(dbname).Collection("videos")
+
 	app := fiber.New()
 
 	app.Get("/video", func(c *fiber.Ctx) error {
-		url := fmt.Sprintf("http://%s:%d/video?path=SampleVideo_1280x720_30mb.mp4", storageHost, storagePort)
+		videoId := c.Query("id")
+		if videoId == "" {
+			return c.SendStatus(404)
+		}
+		objectId, err := primitive.ObjectIDFromHex(videoId)
+		if err != nil {
+			log.Println("Invalid id")
+		}
+
+		var video VideoRecord
+		if err := collection.FindOne(context.TODO(), bson.M{"_id": objectId}).Decode(&video); err != nil {
+			return err
+		}
+
+		url := fmt.Sprintf("http://%s:%d/video?path=%s", storageHost, storagePort, video.Path)
 
 		client := &http.Client{}
 		req, err := http.NewRequest("GET", url, nil)
